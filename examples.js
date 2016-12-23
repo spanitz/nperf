826f7699bf12257d124820bd5cd2baf4f34ed06d
Stefan Panitz <info@stefanpanitz.de>
2016-12-23 13:28:53 +0100

    wip

--- examples.js
+++ examples.js
@@ -1,45 +1,68 @@
 const nperf = require('.');
+const _ = require('lodash');
+
+// nperf()
+//     .test('Spread-Operator', () => {
+//         const b = [4, 5, 6];
+//         [1, 2, 3, ...b];
+//     })
+//     .test('Array.prototype.concat', () => {
+//         const a = [1, 2, 3];
+//         a.concat([4, 5, 6]);
+//     })
+//     .test('Array.prototype.push', () => {
+//         const a = [1, 2, 3];
+//         a.push(4);
+//         a.push(5);
+//         a.push(6);
+//     })
+//     .run();
 
 nperf()
-    .test('Spread-Operator', () => {
-        const b = [4, 5, 6];
-
-        [1, 2, 3, ...b];
+    .test('for', () => {
+        const b = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+        for(let i = 0; i < b.length; i++) {
+            b[i]++;
+        }
     })
-    .test('Array.prototype.concat', () => {
-        const a = [1, 2, 3];
-
-        a.concat([4, 5, 6]);
+    .test('while', () => {
+        const b = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+        let i = b.length;
+        while(i--) {
+            b[i]++;
+        }
     })
-    .test('Array.prototype.push', () => {
-        const a = [1, 2, 3];
-
-        a.push(4);
-        a.push(5);
-        a.push(6);
+    .test('Array.prototype.forEach', () => {
+        const b = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+        b.forEach(x => {
+            x++;
+        });
     })
-    .run();
-
-nperf()
-    .test('Template string', () => {
-        const a = 'foo';
-        const b = 'bar';
-        const c = 'baz';
-
-        `${a} ${b} ${c}`;
-    })
-    .test('String concat', () => {
-        const a = 'foo';
-        const b = 'bar';
-        const c = 'baz';
-
-        a + ' ' + b + ' ' + c;
-    })
-    .test('Array.prototype.join', () => {
-        const a = 'foo';
-        const b = 'bar';
-        const c = 'baz';
-
-        [a, b, c].join(' ');
+    .test('_.forEach', () => {
+        const b = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+        _.forEach(b, x => {
+            x++;
+        });
     })
     .run();
+//
+// nperf()
+//     .test('Template string', () => {
+//         const a = 'foo';
+//         const b = 'bar';
+//         const c = 'baz';
+//         `${a} ${b} ${c}`;
+//     })
+//     .test('String concat', () => {
+//         const a = 'foo';
+//         const b = 'bar';
+//         const c = 'baz';
+//         a + ' ' + b + ' ' + c;
+//     })
+//     .test('Array.prototype.join', () => {
+//         const a = 'foo';
+//         const b = 'bar';
+//         const c = 'baz';
+//         [a, b, c].join(' ');
+//     })
+//     .run();
--- index.js
+++ index.js
@@ -2,13 +2,21 @@ const _ = require('lodash');
 const pretty = require('pretty-time');
 const chalk = require('chalk');
 const util = require('util');
+const cpus = require('os').cpus().length;
+const stream = process.stderr;
+stream.clearLine();
+
+const progress = (value, length, char) => {
+    return `${_.repeat(chalk.white('█'), Math.floor(length * value / 100))}${_.repeat(chalk.gray('█'), length - Math.floor(length * value / 100))} ${value.toFixed(1)}%`;
+}
+
 
 /**
  * Constructor
  *
  * @param  {integer} samples Defines the number of samples nperf should collect for each test; defualts to 1000000
  */
-function nperf(samples = 1000000) {
+function nperf(samples = 10000) {
     this.tests = [];
     this.samples = samples;
 }
@@ -34,27 +42,107 @@ proto.test = function(desc, fn) {
  * @return {nperf}
  */
 proto.run = function() {
-    if (this.tests.length === 0) return this;
+    let i = this.tests.length;
+    if (i === 0) return this;
 
-    this.tests.forEach(x => {
-        const start = process.hrtime();
+    //this.tests.forEach(x => this._run(x.desc, x.fn));
 
-        let i = this.samples;
-        while (i--) {
-            x.fn();
-        }
+    const next = () => {
+        i--;
+        this._run(this.tests[i].desc, this.tests[i].fn).then(() => {
+            if (i > 0) {
+                next();
+            }
+        })
+    };
 
-        x.time = process.hrtime(start);
-        x.avg = (x.time[0] * 1e9 + x.time[1]) / this.samples;
-    });
-
-    const avg = this.tests.map(x => x.avg);
-    this.min = _.min(avg);
-    this.max = _.max(avg);
-
-    this.log();
+    next();
 
     return this;
+}
+
+proto._run = function (desc, fn) {
+    if (global.gc) {
+        stream.write('Collecting garbage...');
+        global.gc();
+        stream.cursorTo(0);
+        stream.clearLine(1);
+    }
+
+    console.log(fn.toString().replace(/(^(\(.*\)|[a-zA-Z]+)\s*=>\s*\{)|(function\s*\(.*\)\s*{)|(}$)/g, ''));
+
+    return new Promise((resolve) => {
+        let i = 1;
+        const samples = 100;
+        const defer = 10;
+        const concurrency = 100;
+        const sampleTime = [];
+        const sampleMem = [];
+        let sumCpuUsage = 0;
+
+        const collectSamples = () => {
+            stream.cursorTo(0);
+            stream.write(`Collecting samples... ${progress(i / samples * 100, 25)}`);
+            stream.clearLine(1);
+
+            let j = concurrency;
+            const sampleStartTime = process.hrtime();
+            while(j--) {
+                fn();
+            }
+            const sampleEndTime = process.hrtime(sampleStartTime);
+
+            const endTime = process.hrtime(startTime);
+            const endCpu = process.cpuUsage(startCpu);
+
+            sampleTime.push((sampleEndTime[0] * 1e9 + sampleEndTime[1]) / concurrency);
+            sampleMem.push(process.memoryUsage().heapUsed);
+
+            const currentTime = (endTime[0] * 1e9 + endTime[1]);
+            const currentCpuTime = endCpu.user * 1e3;
+            const currentCpuUsage = endCpu.user * 1e3 / currentTime * 100;
+
+            sumCpuUsage += currentCpuUsage;
+
+            if (i === samples) {
+                resolve();
+
+                process.nextTick(() => {
+                    stream.cursorTo(0);
+                    stream.clearLine(1);
+                    stream.write(JSON.stringify({
+                        desc,
+                        samples,
+                        totalTime: pretty(currentTime, 3),
+                        totalCpuTime: pretty(currentCpuTime, 3),
+                        avgCpuUsage: sumCpuUsage / samples,
+                        opsPerSec: 1e9 / (sampleTime.reduce((a, b) => a + b) / samples),
+                        avgExecTime: pretty(sampleTime.reduce((a, b) => a + b) / samples, 3),
+                        minExecTime: pretty(_.min(sampleTime), 3),
+                        maxExecTime: pretty(_.max(sampleTime), 3),
+                        avgHeapUsed: sampleMem.reduce((a, b) => a + b) / samples,
+                        minHeapUsed: _.min(sampleMem),
+                        maxHeapUsed: _.max(sampleMem),
+                    }, undefined, 2) + '\n');
+                });
+
+                return;
+            }
+
+            i++;
+            setTimeout(collectSamples, defer);
+
+            process.nextTick(() => {
+                stream.cursorTo(0);
+                stream.write(`Collecting samples... ${progress(i / samples * 100, 25)}`);
+                stream.clearLine(1);
+            });
+        };
+
+        const startTime = process.hrtime();
+        const startCpu = process.cpuUsage();
+        collectSamples();
+    });
 }
 
 /**
@@ -69,11 +157,12 @@ proto.log = function() {
 
     this.tests.forEach((x, i) => {
         const color = x.avg === this.min ? chalk.green.bold : x.avg === this.max ? chalk.red : chalk.gray;
-        this._log.push(util.format((x.avg === this.min ? '%s. %s ' : chalk.gray('%s. %s ')) + color('%sx') + chalk.gray(' (avg ~%s)'),
+        this._log.push(util.format((x.avg === this.min ? '%s. %s ' : chalk.gray('%s. %s ')) + color('%sx') + chalk.gray(' (avg ~%s; ops %s)'),
             i + 1,
             x.desc,
-            Math.round(this.max/x.avg),
-            pretty(x.avg, 3)
+            (this.max / x.avg).toFixed(1),
+            pretty(x.avg, 3),
+            Math.round(1e9 / (x.time / this.samples))
         ));
     });
 
